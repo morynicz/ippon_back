d9c13905206407075970827d72743bc379e48448
Michał Orynicz <morynicz@gmail.com>
2018-06-02 21:58:49 +0200

    Add permisions for tournament

--- ippon/permissions.py
+++ ippon/permissions.py
@@ -1,10 +1,9 @@
 from rest_framework import permissions
 
-from ippon.models import ClubAdmin
+from ippon.models import ClubAdmin, TournamentAdmin
 
 
 class IsClubAdminOrReadOnlyClub(permissions.BasePermission):
-
     def has_object_permission(self, request, view, club):
         if request and request.method in permissions.SAFE_METHODS:
             return True
@@ -15,4 +14,11 @@ class IsClubAdminOrReadOnlyDependent(permissions.BasePermission):
     def has_object_permission(self, request, view, player):
         if request and request.method in permissions.SAFE_METHODS:
             return True
-        return ClubAdmin.objects.all().filter(user=request.user, club=player.club)
+        return ClubAdmin.objects.all().filter(user=request.user, club=player.club_id)
+
+
+class IsTournamentAdminOrReadOnlyTournament(permissions.BasePermission):
+    def has_object_permission(self, request, view, tournament):
+        if request and request.method in permissions.SAFE_METHODS:
+            return True
+        return TournamentAdmin.objects.all().filter(user=request.user, tournament=tournament)
--- ippon/test_permissions.py
+++ ippon/test_permissions.py
@@ -2,7 +2,7 @@ import unittest
 import unittest.mock
 
 from ippon import permissions
-from ippon.permissions import IsClubAdminOrReadOnlyClub
+from ippon.permissions import IsClubAdminOrReadOnlyClub, IsTournamentAdminOrReadOnlyTournament
 
 
 class ClubPermissionsTests(unittest.TestCase):
@@ -14,6 +14,7 @@ class ClubPermissionsTests(unittest.TestCase):
         self.club_admin_objects = patcher.start()
         self.addCleanup(patcher.stop)
         self.permission = IsClubAdminOrReadOnlyClub()
+
 
 class ClubPermissionTestsNotAdmin(ClubPermissionsTests):
     def setUp(self):
@@ -31,6 +32,7 @@ class ClubPermissionTestsNotAdmin(ClubPermissionsTests):
         self.assertEqual(result, False)
         self.club_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user, club=self.club)
 
+
 class ClubPermissionTestsAdmin(ClubPermissionsTests):
     def setUp(self):
         super(ClubPermissionTestsAdmin, self).setUp()
@@ -42,6 +44,7 @@ class ClubPermissionTestsAdmin(ClubPermissionsTests):
         self.assertEqual(result, True)
         self.club_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user, club=self.club)
 
+
 class PlayerPermissionsTest(unittest.TestCase):
     def setUp(self):
         self.player = unittest.mock.Mock()
@@ -51,6 +54,7 @@ class PlayerPermissionsTest(unittest.TestCase):
         self.club_admin_objects = patcher.start()
         self.addCleanup(patcher.stop)
         self.permission = permissions.IsClubAdminOrReadOnlyDependent()
+
 
 class PlayerPermissionTestsNotAdmin(PlayerPermissionsTest):
     def setUp(self):
@@ -66,4 +70,47 @@ class PlayerPermissionTestsNotAdmin(PlayerPermissionsTest):
         self.request.method = 'PUT'
         result = self.permission.has_object_permission(self.request, self.view, self.player)
         self.assertEqual(result, False)
-        self.club_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user, club=self.player.club)
+        self.club_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user,
+                                                                           club=self.player.club_id)
+
+
+class TournamentPermissions(unittest.TestCase):
+    def setUp(self):
+        self.tournament = unittest.mock.Mock()
+        self.request = unittest.mock.Mock()
+        self.view = unittest.mock.Mock()
+        patcher = unittest.mock.patch("ippon.models.TournamentAdmin.objects")
+        self.tournament_admin_objects = patcher.start()
+        self.addCleanup(patcher.stop)
+        self.permission = IsTournamentAdminOrReadOnlyTournament()
+
+
+class TournamentPermissionTestsNotAdmin(TournamentPermissions):
+    def setUp(self):
+        super(TournamentPermissionTestsNotAdmin, self).setUp()
+        self.tournament_admin_objects.all.return_value.filter.return_value = False
+
+    def test_permits_when_safe_method(self):
+        self.request.method = 'GET'
+        result = self.permission.has_object_permission(self.request, self.view, self.tournament)
+        self.assertEqual(result, True)
+
+    def test_doesnt_permit_when_unsafe_method(self):
+        self.request.method = 'PUT'
+        result = self.permission.has_object_permission(self.request, self.view, self.tournament)
+        self.assertEqual(result, False)
+        self.tournament_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user,
+                                                                                 tournament=self.tournament)
+
+
+class TournamentPermissionTestsAdmin(TournamentPermissions):
+    def setUp(self):
+        super(TournamentPermissionTestsAdmin, self).setUp()
+        self.tournament_admin_objects.all.return_value.filter.return_value = True
+
+    def test_doesnt_permit_when_unsafe_method(self):
+        self.request.method = 'PUT'
+        result = self.permission.has_object_permission(self.request, self.view, self.tournament)
+        self.assertEqual(result, True)
+        self.tournament_admin_objects.all.return_value.filter.assert_called_with(user=self.request.user,
+                                                                                 tournament=self.tournament)
