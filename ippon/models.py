9d345cd52d17622c0bf9fa6d44594ae0b96553e1
Michał Orynicz <morynicz@gmail.com>
2018-06-02 21:27:01 +0200

    Add Tournament and TournamentAdmin

--- ippon/models.py
+++ ippon/models.py
@@ -44,3 +44,45 @@ class Player(models.Model):
 class ClubAdmin(models.Model):
     club = models.ForeignKey('Club', related_name='admins', on_delete=models.PROTECT)
     user = models.ForeignKey('auth.User', related_name='clubs', on_delete=models.PROTECT)
+
+
+NUMERIC_CONSTRAINT = [
+    (0, 'None'),
+    (1, 'Less'),
+    (2, 'LessOrEqual'),
+    (3, 'Greater'),
+    (4, 'GreateOrEqual'),
+    (5, 'Equal'),
+    (6, 'NotEqual')
+]
+
+SEX_CONSTRAINT = [
+    (0, 'None'),
+    (1, 'WomenOnly'),
+    (2, 'MenOnly')
+]
+
+
+class TournamentAdmin(models.Model):
+    tournament = models.ForeignKey('Tournament', related_name='admins', on_delete=models.PROTECT)
+    user = models.ForeignKey('auth.User', related_name='tournaments', on_delete=models.PROTECT)
+    is_master = models.BooleanField()
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    webpage = models.URLField()
+    description = models.TextField()
+    city = models.CharField(max_length=100, blank=False)
+    date = models.DateField()
+    address = models.CharField(max_length=500, blank=False)
+    team_size = models.IntegerField()
+    group_match_length = models.IntegerField()
+    ko_match_length = models.IntegerField()
+    final_match_length = models.IntegerField()
+    age_constraint = models.IntegerField(choices=NUMERIC_CONSTRAINT)
+    rank_constraint = models.IntegerField(choices=NUMERIC_CONSTRAINT)
+    sex_constraint = models.IntegerField(choices=SEX_CONSTRAINT)
+    rank_constraint_value = models.IntegerField(choices=RANK_CHOICES)
+    age_constraint_value = models.IntegerField()
+    finals_depth = models.IntegerField()
--- ippon/serializers.py
+++ ippon/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ippon.models import Club, Player
+from ippon.models import Club, Player, Tournament
 
 
 class ClubSerializer(serializers.ModelSerializer):
@@ -13,3 +13,26 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = ('id', 'name', 'surname', 'rank', 'sex', 'birthday', 'club_id')
+
+class TournamentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Tournament
+        fields = (
+            'id',
+            'name',
+            'date',
+            'city',
+            'address',
+            'description',
+            'webpage',
+            'team_size',
+            'group_match_length',
+            'ko_match_length',
+            'final_match_length',
+            'finals_depth',
+            'age_constraint',
+            'sex_constraint',
+            'rank_constraint',
+            'rank_constraint_value',
+            'age_constraint_value'
+        )
