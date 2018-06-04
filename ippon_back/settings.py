68759b798cc1bdbded98f0676053aec0eea28e68
Michał Orynicz <morynicz@gmail.com>
2018-06-04 21:34:21 +0200

    Allow multiple deployment hosts

--- ippon_back/settings.py
+++ ippon_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.9', '192.168.1.11', 'ippon-backend.herokuapp.com']
 
 # Application definition
 
