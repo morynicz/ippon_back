f8417d7d803da1a58b7166210e364298fb176b3a
Michał Orynicz <morynicz@gmail.com>
2018-12-29 14:57:22 +0100

    I don't remember why was the token modified, but turn mod off

--- auth_server/serializers.py
+++ auth_server/serializers.py
@@ -7,6 +7,6 @@ class UserTokenSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(UserTokenSerializer, cls).get_token(user)
-        token['expires_at'] = 1234
+        # token['expires_at'] = 1234
 
         return token
