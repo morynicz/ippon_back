241dd5d50eff74021d3955f075ae2c1675bc40dd
Michał Orynicz <morynicz@gmail.com>
2019-08-04 16:53:04 +0200

    Add stub for secrets generator

---
+++ gen_secrets.sh.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+
+TEMPLATE="""
+#!/bin/bash
+
+export SECRET_KEY='{key}'
+export DB_NAME='{dbname}'
+export DB_USER='{dbuser}'
+export DB_PASSWORD='{dbpass}'
+export DB_HOST='localhost'
+export DB_PORT=''
+export SECRET_KEY_PRIVATE=`cat {keypath}/id_rsa`
+export SECRET_KEY_PUBLIC=`cat {keypath}/id_rsa.pub`
+export MAIL_USER=''
+export MAIL_PASSWORD=''
+export MAIL_HOST=''
+export MAIL_PORT=''
+export MAIL_USE_TLS='True'
+export MAIL_USE_SSL=''
+export MAIL_SSL_KEYFILE=''
+export MAIL_SSL_CERTFILE=''"""
+
+def gen_ssh(dir):
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption())
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.OpenSSH,
+        crypto_serialization.PublicFormat.OpenSSH
+    )
+
+    with open(os.path.join(dir, 'id_rsa'), 'wb') as private:
+        private.write(private_key)
+
+    with open(os.path.join(dir, 'id_rsa.pub'), 'wb') as public:
+        public.write(public_key)
+
+
+def gen_key():
+    return "1234"
+
+
+def generate():
+    with open("secrets2.sh", "w") as file:
+        dir = os.path.join(os.getcwd(),"keys")
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        # keygen_invokation = ["ssh-keygen", "-f {}".format(os.path.join(dir, "id_rsa")), '-q', '-P' ,str()]
+        # # print(subprocess.check_output(keygen_invokation))
+        # subprocess.run(keygen_invokation)
+        gen_ssh(dir)
+        file.write(TEMPLATE.format(key=gen_key(), dbname="dbname", dbuser="dbuser", dbpass="dbpass", keypath=dir))
+
+
+if __name__ == '__main__':
+    generate()
