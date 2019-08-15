e79c7b9043929237158497337b02ff6f6e1967da
Michał Orynicz <morynicz@gmail.com>
2019-08-15 19:27:47 +0200

    Further attempts with selenium, still not working

--- e2e/test_e2e.py
+++ e2e/test_e2e.py
@@ -1,11 +1,15 @@
+import selenium
 from django.contrib.auth.models import User
 from django.test import LiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
+
+import ippon
 
 
 class MySeleniumTests(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
+        cls.port = 8923
         super().setUpClass()
         cls.selenium = WebDriver()
         cls.selenium.implicitly_wait(10)
@@ -15,21 +19,40 @@ class MySeleniumTests(LiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_login(self):
-        email = 'admin'
-        password = 'password'
-        self.user = User.objects.create(username='admin', password='password')
+    def setUp(self) -> None:
+        super(MySeleniumTests, self).setUp()
+        self.username = 'admin'
+        self.password = 'password'
+        self.email= 'admin@amdin.com'
+        self.user = User.objects.create_user(username=self.username, password=self.password, email=self.email)
 
+    def log_in(self):
         self.selenium.get('%s%s' % ('localhost:4231', '/login/'))
-        username_input = self.selenium.find_element_by_name("email")
-        username_input.send_keys(email)
-        password_input = self.selenium.find_element_by_name("password")
-        password_input.send_keys(password)
-        self.selenium.find_element_by_id('login-login').click()
-
         self.selenium.implicitly_wait(10)
 
-        self.assertIsNone(self.selenium.find_element_by_id('logout'))
+        username_input = self.selenium.find_element_by_name("email")
+        username_input.send_keys(self.username)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(self.password)
+        self.selenium.implicitly_wait(10)
+        self.selenium.find_element_by_id('login-login').click()
 
-# class E2ETests(test.TestCase):
-#     pass
+
+    def test_login(self):
+        self.log_in()
+        # self.selenium.implicitly_wait(100)
+        with self.assertRaises(selenium.common.exceptions.NoSuchElementException):
+            self.selenium.find_element_by_id("login")
+
+    def test_logout(self):
+        self.log_in()
+        # self.selenium.implicitly_wait(100)
+
+        print(self.selenium.find_element_by_id('logout'))
+        self.selenium.find_element_by_id('logout').click()
+
+        with self.assertRaises(selenium.common.exceptions.NoSuchElementException):
+            self.selenium.find_element_by_id("logout")
+
+
+
