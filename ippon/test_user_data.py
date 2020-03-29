901f88a5ac20e0073cd93c5518bd9de664823753
CreativePhilip <f.policki@gmail.com>
2020-03-29 22:15:01 +0200

    fixed test names to adhere to project level guidelines

--- ippon/test_user_data.py
+++ ippon/test_user_data.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.models import User
 from django.urls import reverse
 from requests import Response
+from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 
@@ -10,13 +11,15 @@ class UserDataViewTest(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
 
-    def test_invalid_user_response(self):
+    def test_invalid_user_responds_with_401_and_error_message(self):
         response: Response = self.client.get(reverse("user-data"))
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data, {
             "error": "You are not logged in."
         })
 
-    def test_valid_user_response(self):
+    def test_valid_user_responds_with_200_and_correct_data(self):
         self.user = User.objects.create_user(
             email="jack@ripper.com",
             username="JackTheRipper",
@@ -27,6 +30,8 @@ class UserDataViewTest(APITestCase):
         self.client.force_authenticate(self.user)
 
         response: Response = self.client.get(reverse("user-data"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
             "id": self.user.id,
             "first_name": self.user.first_name,
