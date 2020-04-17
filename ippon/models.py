9f54d6a58856d6b1f838ba990d4765e9f169a5f5
Philip <creativepyt@gmail.com>
2020-04-17 20:23:34 +0200

    changed import type

--- ippon/models.py
+++ ippon/models.py
@@ -8,8 +8,7 @@ from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-from .event_models import Event
+from ippon.event_models import Event
 
 
 class Club(models.Model):
