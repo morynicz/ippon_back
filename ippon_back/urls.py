79a54b4f53db646c1d0a85a2e9d3811924d07754
philip <creativepyt@gmail.com>
2020-07-16 21:23:05 +0200

    returning static files by the server

--- ippon_back/urls.py
+++ ippon_back/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from ippon_back import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -40,4 +43,4 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
